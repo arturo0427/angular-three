ec6479d66f8f9a495b13d5af191b0e40effc127d
arturo0427 <arturo_munoz27@outlook.com>
2025-04-25 16:46:23 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgtCanvas } from 'angular-three';
+import { SceneGraph } from '../scene-graph';
+import { Color } from 'three';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgtCanvas],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: { class: 'block h-dvh w-full' },
 })
 export class AppComponent {
   title = 'angular-three';
+
+  protected sceneGraph = SceneGraph;
+  backgroundColor = new Color('#121212');
 }
