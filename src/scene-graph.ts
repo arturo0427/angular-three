4d7a5752cac5b4f91adb317319674b449a27ec24
arturo0427 <arturo_munoz27@outlook.com>
2025-04-28 11:37:05 -0500

    feat: lights and orbit control

--- src/scene-graph.ts
+++ src/scene-graph.ts
@@ -1,29 +1,75 @@
 import {
   CUSTOM_ELEMENTS_SCHEMA,
-  Component,
-  viewChild,
-  ElementRef,
   ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  viewChild,
 } from '@angular/core';
-import { extend, injectBeforeRender } from 'angular-three';
-import { Mesh, BoxGeometry, MeshBasicMaterial } from 'three';
+import {
+  NgtArgs,
+  extend,
+  injectBeforeRender,
+  injectStore,
+} from 'angular-three';
+import {
+  AmbientLight,
+  BoxGeometry,
+  DirectionalLight,
+  GridHelper,
+  Mesh,
+  MeshStandardMaterial,
+  SpotLight,
+} from 'three';
+import { OrbitControls } from 'three-stdlib';
 
 @Component({
   selector: 'app-scene-graph',
   template: `
+    <!-- Mesh -->
     <ngt-mesh #mesh>
       <ngt-box-geometry />
-      <ngt-mesh-basic-material color="hotpink" />
+      <ngt-mesh-standard-material color="hotpink" />
     </ngt-mesh>
+
+    <!-- Luces a nivel de escena -->
+    <ngt-ambient-light [intensity]="0.1" />
+    <ngt-directional-light
+      [position]="10"
+      [intensity]="0.7 * Math.PI"
+      [angle]="0.4"
+      [penumbra]="1"
+      [decay]="0"
+      [color]="'#1a1a'"
+    />
+
+    <!-- OrbitControls: args = [camera, gl.domElement] -->
+    <ngt-orbit-controls *args="[camera(), glDomElement()]" />
+    <ngt-grid-helper />
   `,
+  imports: [NgtArgs],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SceneGraph {
   private meshRef = viewChild.required<ElementRef<Mesh>>('mesh');
+  protected Math = Math;
+
+  private store = injectStore();
+
+  protected camera = this.store.select('camera');
+  protected glDomElement = this.store.select('gl', 'domElement');
 
   constructor() {
-    extend({ Mesh, BoxGeometry, MeshBasicMaterial });
+    extend({
+      Mesh,
+      BoxGeometry,
+      MeshStandardMaterial,
+      AmbientLight,
+      SpotLight,
+      DirectionalLight,
+      OrbitControls,
+      GridHelper,
+    });
 
     injectBeforeRender(({ delta }) => {
       const mesh = this.meshRef().nativeElement;
